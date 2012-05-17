60058745c40dd491b0c2762013bcbb60ef0b1812
Hsu <hmhlaing@hotmail.com>
2012-05-17 15:07:53 +0100

    adding code for checking neighbours. Tests for dead cell features now pass. Added the rule fo any dead cell with 3 neighbours becomes alive. Still a bunch of mess and needs refectoring

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -1,9 +1,122 @@
 class GameOfLife
 
   def next_step(new_table, generations = 1)
+
+    #make a copy for next generation
+    next_generation_table = new_table
+
+    #More rigorous checks to be added
+    @max_row = new_table.length-1
+    @max_col = new_table[0].length-1
+
     if generations == 1
-      new_table
+      for row in 0..new_table.length-1
+        for col in 0..new_table[row].length-1
+          dead_count = 0
+          live_count = 0
+          #item for inspection
+          cell_initial_state = new_table[row][col]
+
+          #To refactor check neighbours
+          #row-1, col
+          if checkBordersOK(row-1, col)
+            if new_table[row-1][col] == '.'
+              dead_count += 1
+            else
+              live_count += 1
+            end
+          end
+
+          #row+1, col
+          if checkBordersOK(row+1, col)
+            if new_table[row+1][col] == '.'
+              dead_count += 1
+            else
+               live_count += 1
+            end
+          end
+
+          #row, col-1
+          if checkBordersOK(row, col-1)
+            if new_table[row][col-1] == '.'
+              dead_count += 1
+            else
+               live_count += 1
+            end
+          end
+
+          #row, col+1
+          if checkBordersOK(row, col+1)
+            if new_table[row][col+1] == '.'
+              dead_count += 1
+            else
+               live_count += 1
+            end
+          end
+
+          #row-1, col-1
+          if checkBordersOK(row-1, col-1)
+            if new_table[row-1][col-1] == '.'
+              dead_count += 1
+            else
+               live_count += 1
+            end
+          end
+
+          #row-1, col+1
+          if checkBordersOK(row-1, col+1)
+            if new_table[row-1][col+1] == '.'
+              dead_count += 1
+            else
+               live_count += 1
+            end
+          end
+
+          #row+1, col-1
+          if checkBordersOK(row+1, col-1)
+            if new_table[row+1][col-1] == '.'
+              dead_count += 1
+            else
+               live_count += 1
+            end
+          end
+
+          #row+1, col+1
+          if checkBordersOK(row+1, col+1)
+            if new_table[row+1][col+1] == '.'
+              dead_count += 1
+            else
+               live_count += 1
+            end
+          end
+
+          #Any live cell with fewer than two live neighbours dies
+          #Any live cell with two or three live neighbours lives
+          #Any live cell with more than three live neighbours dies
+
+          #Any dead cell with exactly three live neighbours becomes a live cell
+          if live_count == 3
+            #if the cell is live initially it will still live
+            next_generation_table[row][col] = 'x'
+          end
+       end # end for col
+      end #end for row
+    end #end if generations
+
+
+    for row in 0..new_table.length-1
+      for col in 0..new_table[row].length-1
+        puts next_generation_table[row][col]
+      end
+        puts "####"
     end
+
+    return next_generation_table
+  end #end def
+
+
+  def checkBordersOK(row, col)
+    !(row < 0) && !(col<0) && !(row> @max_row) && !(col>@max_col)
   end
 
 end
