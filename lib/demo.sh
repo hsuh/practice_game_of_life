4d9952c755016826f9e7d397eaf026111ca9900d
Hsu <hmhlaing@hotmail.com>
2012-05-18 15:04:42 +0100

    Added more comments and cleaned up the code a bit

--- lib/demo.sh
+++ lib/demo.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env ruby
-#You can run this file by typing ./demo.sh in terminal (you might have to make this file +x)
-#Feel free to change the parameters
+
+################################################################################
+#You can run this file by typing ./demo.sh in terminal (you might have to make
+#this file chmod +x)
+#Feel free to change the parameters.
+#
+#Input file             - glidergun.world (Or create your own!)
+#Delay/Pause            - 0.5 seconds
+#Number of generations  - 500
+################################################################################
 
 load 'playLife.rb'
-
-#glidergun is in the same folder. You can create your own *.world files!
 PlayLife.load_world('glidergun.world')
 
-#Playlife.runLife(pause_time, Number of generations)
+#Parameters (delay in seconds, Number of generations)
 PlayLife.runLife(0.1,500)
--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -1,5 +1,8 @@
 class GameOfLife
 
+  ALIVE = 'x'
+  DEAD  = '.'
+
   def initialize(data)
     @new_generation = data
   end
@@ -13,76 +16,62 @@ class GameOfLife
     @max_row = @new_generation.length-1
     @max_col = @new_generation.first.length-1
 
+    #At the moment, we don't care if the inspected cells are dead, alive or valid
+    #cell validation to be added
     @new_generation.each_with_index do |row, y|
       row.each_with_index do |col, x|
         live_count = 0
 
         #checking the neighbour above
-        if checkBordersOK(y-1, x)
-          if @new_generation[y-1][x] == 'x'
+        if checkBordersOK(y-1, x) && @new_generation[y-1][x] == ALIVE
             live_count += 1
-          end
         end
 
         #below, same column
-        if checkBordersOK(y+1, x)
-          if @new_generation[y+1][x] == 'x'
+        if checkBordersOK(y+1, x) && @new_generation[y+1][x] == ALIVE
             live_count += 1
-          end
         end
 
         #left, same row
-        if checkBordersOK(y, x-1)
-          if @new_generation[y][x-1] == 'x'
+        if checkBordersOK(y, x-1) && @new_generation[y][x-1] == ALIVE
               live_count += 1
-          end
         end
 
         #right, same row
-        if checkBordersOK(y, x+1)
-          if @new_generation[y][x+1] == 'x'
+        if checkBordersOK(y, x+1) && @new_generation[y][x+1] == ALIVE
             live_count += 1
-          end
         end
 
         #top left
-        if checkBordersOK(y-1, x-1)
-          if @new_generation[y-1][x-1] == 'x'
+        if checkBordersOK(y-1, x-1) && @new_generation[y-1][x-1] == ALIVE
             live_count += 1
-          end
         end
 
         #top right
-        if checkBordersOK(y-1, x+1)
-          if @new_generation[y-1][x+1] == 'x'
+        if checkBordersOK(y-1, x+1) && @new_generation[y-1][x+1] == ALIVE
             live_count += 1
-          end
         end
 
         #bottom left
-        if checkBordersOK(y+1, x-1)
-          if @new_generation[y+1][x-1] == 'x'
+        if checkBordersOK(y+1, x-1) && @new_generation[y+1][x-1] == ALIVE
             live_count += 1
-          end
         end
 
         #bottom right
-        if checkBordersOK(y+1, x+1)
-          if @new_generation[y+1][x+1] == 'x'
+        if checkBordersOK(y+1, x+1) && @new_generation[y+1][x+1] == ALIVE
             live_count += 1
-          end
         end
 
         #Any live cell with fewer than two live neighbours dies
         #Any live cell with two or three live neighbours lives
         #Any live cell with more than three live neighbours dies
         if live_count < 2 || live_count > 3
-          @next_generation[y][x] = '.'
+          @next_generation[y][x] = DEAD
         end
 
         #Any dead or live cell with exactly three live neighbours is a live cell
         if live_count == 3
-          @next_generation[y][x] = 'x'
+          @next_generation[y][x] = ALIVE
         end
        end # end for col
       end #end for row
