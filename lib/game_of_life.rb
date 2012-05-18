941e3fd61ac7638f5b7f522336faf6cd0e8803c3
Hsu <hmhlaing@hotmail.com>
2012-05-18 20:02:08 +0100

    made code look nicer

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -9,7 +9,7 @@ class GameOfLife
 
   def next_generation
 
-    #make a copy for next generation
+    #Make a copy for next generation
     @next_generation = Marshal.load(Marshal.dump(@new_generation))
 
     #More rigorous checks should be added
@@ -17,77 +17,85 @@ class GameOfLife
     @max_col = @new_generation.first.length-1
 
     #At the moment, we don't care if the inspected cells are dead, alive or valid
-    #cell validation to be added
+    #Cell validation to be added
     @new_generation.each_with_index do |row, y|
       row.each_with_index do |col, x|
-        live_count = 0
+        live_neighbours = 0
+        live_neighbours = countNeighbours(y,x)
 
-        #checking the neighbour above
-        if checkBordersOK(y-1, x) && @new_generation[y-1][x] == ALIVE
-            live_count += 1
-        end
-
-        #below, same column
-        if checkBordersOK(y+1, x) && @new_generation[y+1][x] == ALIVE
-            live_count += 1
-        end
-
-        #left, same row
-        if checkBordersOK(y, x-1) && @new_generation[y][x-1] == ALIVE
-              live_count += 1
-        end
-
-        #right, same row
-        if checkBordersOK(y, x+1) && @new_generation[y][x+1] == ALIVE
-            live_count += 1
-        end
-
-        #top left
-        if checkBordersOK(y-1, x-1) && @new_generation[y-1][x-1] == ALIVE
-            live_count += 1
-        end
-
-        #top right
-        if checkBordersOK(y-1, x+1) && @new_generation[y-1][x+1] == ALIVE
-            live_count += 1
-        end
-
-        #bottom left
-        if checkBordersOK(y+1, x-1) && @new_generation[y+1][x-1] == ALIVE
-            live_count += 1
-        end
-
-        #bottom right
-        if checkBordersOK(y+1, x+1) && @new_generation[y+1][x+1] == ALIVE
-            live_count += 1
-        end
-
-        #Any live cell with fewer than two live neighbours dies
-        #Any live cell with two or three live neighbours lives
-        #Any live cell with more than three live neighbours dies
-        if live_count < 2 || live_count > 3
+        #Live cell with fewer than two live neighbours dies
+        #Live cell with two or three live neighbours lives
+        #Live cell with more than three live neighbours dies
+        if live_neighbours < 2 || live_neighbours > 3
           @next_generation[y][x] = DEAD
         end
 
-        #Any dead or live cell with exactly three live neighbours is a live cell
-        if live_count == 3
+        #Dead or live cell with exactly three live neighbours becomes/is alive
+        if live_neighbours == 3
           @next_generation[y][x] = ALIVE
         end
-       end # end for col
-      end #end for row
+
+       end #Each col
+      end #Each row
 
     @new_generation = @next_generation
-  end #end def
+  end #End def
 
   def get_next_generation
     @next_generation
   end
 
-  #helper for diplaying the worlds nicely
+  #Helper for diplaying the worlds nicely
   def displayLife
     @next_generation.each_with_index do |row, y|
         puts row.map { |i| "" + i.to_s + "" }.join()
     end
+  end
+
+  def countNeighbours(y,x)
+
+    neighbours = 0
+    #Checking the neighbour above
+    if checkBordersOK(y-1, x) && @new_generation[y-1][x] == ALIVE
+      neighbours += 1
+    end
+
+    #Below, same column
+    if checkBordersOK(y+1, x) && @new_generation[y+1][x] == ALIVE
+      neighbours += 1
+    end
+
+    #Left, same row
+    if checkBordersOK(y, x-1) && @new_generation[y][x-1] == ALIVE
+        neighbours += 1
+    end
+
+    #Right, same row
+    if checkBordersOK(y, x+1) && @new_generation[y][x+1] == ALIVE
+      neighbours += 1
+    end
+
+    #Top left
+    if checkBordersOK(y-1, x-1) && @new_generation[y-1][x-1] == ALIVE
+      neighbours += 1
+    end
+
+    #Top right
+    if checkBordersOK(y-1, x+1) && @new_generation[y-1][x+1] == ALIVE
+      neighbours += 1
+    end
+
+    #Bottom left
+    if checkBordersOK(y+1, x-1) && @new_generation[y+1][x-1] == ALIVE
+      neighbours += 1
+    end
+
+    #Bottom right
+    if checkBordersOK(y+1, x+1) && @new_generation[y+1][x+1] == ALIVE
+      neighbours += 1
+    end
+
+    neighbours
   end
 
   #Check the edge cases, make sure we are not checking nonexistent neighbours
